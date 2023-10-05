95f52e777563f79aef8692d9962aaaefbd0c28fb
Rodrigo Ferrari <ferrari_rod@hotmail.com>
2023-10-05 17:17:02 -0300

    language translation on bs functions

--- src/js/game/bootstrap.js
+++ src/js/game/bootstrap.js
@@ -1,12 +1,24 @@
 import * as bootstrap from "bootstrap";
 
+let langSelector = window.location.href.includes('english') ? 1 : 0;
+const commandsVariantions = [
+    [
+        'Abrir',
+        'Fechar'
+    ],
+    [
+        'Open',
+        'Close'
+    ]
+];
+
 const collpaseFunctions = document.getElementById('collapseFunctions');
 const collapseBtn = document.getElementById('collapseBtn');
 collpaseFunctions.addEventListener('show.bs.collapse',(e) =>{
-    collapseBtn.innerText = "Fechar";
+    collapseBtn.innerText = commandsVariantions[langSelector][1];
 });
 collpaseFunctions.addEventListener('hide.bs.collapse',(e) =>{
-    collapseBtn.innerText = "Abrir";
+    collapseBtn.innerText = commandsVariantions[langSelector][0];
 });
 
 const consoleElement = document.getElementById('consoleArea');
